028bbc915c7d84c2addc5fe35146f79b868bce4c
Jerowe Tan <jerowe.tan99@gmail.com>
2024-06-30 20:14:09 +0800

    fresh

--- src/cache/index.ts
+++
@@ -1,4 +0,0 @@
-import { Cache } from './cache'
-
-export { Cache } from './cache'
-export default new Cache()
--- src/client/gi/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './gi'
-export * from './gi.helper'
-export * from './gi.interface'
-
-// Export module interfaces
-export * from './diary'
-export * from './record'
-export * from './tcg'
